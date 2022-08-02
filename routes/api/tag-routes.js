cad045b764d9e2a898102a998ad95a4affb732d0
Jondo <chet9880@gmail.com>
2022-08-01 23:36:22 -0400

    denoted async functions

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -46,7 +46,7 @@ router.post('/', async (req, res) => {
 }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   try {
     const tagData = await Tag.update(req.body,
       {
@@ -60,7 +60,7 @@ router.put('/:id', (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   try {
     const tagData = await Tag.destroy({
       where: {id: req.params.id}
